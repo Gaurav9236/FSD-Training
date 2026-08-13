3e3f7a9d2eab3550a838451bd7c48196d2abebc1
Gaurav9236 <enggaurav.eg@gmail.com>
2026-08-13 14:40:12 +0530

    first

--- day4/hof.js
+++ day4/hof.js
@@ -1,2 +1,16 @@
 const arr = [1,2,3,4,5,6,7,8,9,10]
 
+const divisibleOfThree = arr.filter((data)=>data%3 == 0); 
+console.log(divisibleOfThree);
+
+
+const firstDivisibleOfThree = arr.filter((data)=>data%3 == 0); 
+console.log(firstDivisibleOfThree);
+
+const  sumOfArr = arr.reduce((data, acc)=>{
+    acc +=data;
+    return acc;
+}, 0);
+console.log(sunOfArr);
+
+
