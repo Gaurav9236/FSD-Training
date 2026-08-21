6bf7708b3680f1c41a805d8936b31a3ab7d677e4
Gaurav9236 <enggaurav.eg@gmail.com>
2026-08-21 10:11:03 +0530

    day 7

--- day6/index.js
+++ day6/index.js
@@ -4,11 +4,24 @@
 // div.append(h1);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const h1 = React.createElement("h1",{},
-    React.createElement("div",{},
-        React.createElement("span",{},"1")
-    )
+// const h1 = React.createElement("h1",{},
+//     React.createElement("div",{},
+//         React.createElement("span",{},"1")
+//     )
+// );
+
+
+
+const div = (
+  <>
+    <div>
+      <span>Content-01</span>
+    </div>
+
+    <div>
+      <span>Content-02</span>
+    </div>
+  </>
 );
+
 root.render(div);
-
-
---
+++ day7/index.js
@@ -0,0 +1,22 @@
+ const root = ReactDOM.creatRoot(document.getElementById("root"))
+
+ const HeaderComponent = () =>{
+
+    return(<div style={{textAlign:"center"}}><h1></h1></div>)
+ }
+
+; const reactComponent = ()=>{
+    return (<div>
+    <div>Product-1</div>
+    <div>Product-2</div>
+    <div>Product-3</div>
+    <div>Product-4</div>
+    <div>Product-5</div>
+    <div>Product-6</div>
+    <div>Product-7</div>
+  
+</div>)
+}
+
+const reactElement2 = <ReactComponent/>
+root.render(reactElement)
