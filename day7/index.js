e839c6c6164c2f354d767c620c282c5fe4334be3
Gaurav9236 <enggaurav.eg@gmail.com>
2026-08-21 10:17:33 +0530

    react without jsx and with jsx

--- day7/index.js
+++ day7/index.js
@@ -1,22 +1,34 @@
- const root = ReactDOM.creatRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
- const HeaderComponent = () =>{
-
-    return(<div style={{textAlign:"center"}}><h1></h1></div>)
- }
-
-; const reactComponent = ()=>{
-    return (<div>
-    <div>Product-1</div>
-    <div>Product-2</div>
-    <div>Product-3</div>
-    <div>Product-4</div>
-    <div>Product-5</div>
-    <div>Product-6</div>
-    <div>Product-7</div>
-  
-</div>)
+const HeaderComponent = () => {
+    return (<div style={{ textAlign: "center" }}><h1>E-commerce Website</h1></div>)
 }
+const ProductComponent = () => {
 
-const reactElement2 = <ReactComponent/>
-root.render(reactElement)
+    return (<div className="prod-container">
+        <div>Product-01</div>
+        <div>Product-02</div>
+        <div>Product-03</div>
+        <div>Product-04</div>
+        <div>Product-05</div>
+        <div>Product-06</div>
+        <div>Product-07</div>
+        <div>Product-01</div>
+        <div>Product-02</div>
+        <div>Product-03</div>
+        <div>Product-04</div>
+        <div>Product-05</div>
+        <div>Product-06</div>
+        <div>Product-07</div>
+    </div>)
+}
+const FooterComponent = () => {
+    return (<div><h1>copyright all rights are reserved...</h1></div>)
+}
+const reactElement = <>
+    <HeaderComponent />
+    <ProductComponent />
+    <FooterComponent />
+</>
+
+root.render(reactElement);
